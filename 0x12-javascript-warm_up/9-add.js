9269477ef1b2c25e87df192ab590972b376193ca
oluwasube <enibeke20@gmail.com>
2022-09-09 02:02:53 +0100

    script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+}
+const num1 = parseInt(process.arg[2]);
+const num2 = parseInt(process.arg[3]);
+if (isNaN(num1) || isNaN(num2)) {
+    console.log('NAN');
+} else {
+    console.log(add(num1, num2));
+}
