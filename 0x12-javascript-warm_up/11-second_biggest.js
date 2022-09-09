1a3f92ce8eb8b68e427876062707e65fc2ce7426
oluwasube <enibeke20@gmail.com>
2022-09-09 02:46:05 +0100

    updated task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length <= 3) {
     console.log('0');
   } else {
-    console.log(process.argv.sort(function (a, b) { return a - b; })[process.argv.length - 2]);
+    console.log(process.argv.sort((a, b) => a - b)[process.argv.length - 2]);
   }
   
