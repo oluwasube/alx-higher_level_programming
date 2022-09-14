41480b2752dc9826c6ff569461879a8012a3c85b
oluwasube <enibeke20@gmail.com>
2022-09-14 12:42:38 +0100

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const ParentSquare = require('./5-square');
+
+ class Square extends ParentSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+};
+
+module.exports = Square;
