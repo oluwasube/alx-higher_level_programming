6237c039b4e8be86eeed5a2f6e863b6973c9a4f1
Oluwasube promise <enibeke20@gmail.com>
2022-09-17 21:51:42 +0100

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
-module.exports =  Rectangle;
