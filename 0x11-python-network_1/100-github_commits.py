e53d01d82b10c18c7235d74d50c73b1a5eb52b1a
oluwasube <enibeke20@gmail.com>
2022-09-24 14:53:44 +0100

    completed

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""list 10 commits from the repository"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    owner = sys.argv[2]
+    repo = sys.argv[1]
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10".format(
+            owner,
+            repo)
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits:
+        sha = commit.get("sha")
+        name = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, name))
