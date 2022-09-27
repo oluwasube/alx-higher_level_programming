74618244279e13ff6b528ae6621ecee835a1f714
Oluwasube promise <enibeke20@gmail.com>
2022-09-27 18:04:40 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""fetch URL and display as required"""
-import requests
+"""Fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
+    import requests
+
     r = requests.get('https://intranet.hbtn.io/status')
+
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
     print('\t- content: {}'.format(r.text))
