fc6a35698d7b184f62b926f84a6b7c7ce6456eea
Oluwasube promise <enibeke20@gmail.com>
2022-09-27 17:17:20 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""fetch https://intranet.hbtn.io/status"""
-import urllib.request
+"""Fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
