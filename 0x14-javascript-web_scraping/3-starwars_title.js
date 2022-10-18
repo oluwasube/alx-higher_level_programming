b9b727a12d653ace4d9df0b45c777bfc24f48935
Oluwasube promise <enibeke20@gmail.com>
2022-10-18 18:28:17 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const axios = require('axios');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-axios.get(url)
-  .then(response => {
-    console.log(response.data.title);
-  }).catch(error => {
-    console.log(error);
+const request = require('request');
+const BASE_URL = 'https://swapi-api.hbtn.io/api';
+
+if (process.argv.length > 2) {
+  request(`${BASE_URL}/films/${process.argv[2]}/`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(JSON.parse(body).title);
   });
